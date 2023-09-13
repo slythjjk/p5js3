0abb8e04604c8c119abf42e4a6ae35cf8adc8bc3
nalu <142427410+slythjjk@users.noreply.github.com>
2023-09-13 12:12:24 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("white");
+  fill ("black");
+  textSize (64);
+  textAlign (CENTER, CENTER);
+  
+  let maximo = width;
+  let minimo = 0;
+  // mouseX, 0, width ==> 0, palavra.length
+  let palavra = "aaaaaaaaaaaa";
+  let quantidade = map(mouseX, 0, width, 1, palavra.length);
+  //console.log(quantidade);
+  let parcial = palavra.substring(0, quantidade);
+  text(parcial, 200, 200);
+}
